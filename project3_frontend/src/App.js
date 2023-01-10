cdc2078c88a1f1c49a68b267b853c240c54bb01e
Eric Er Chin Ann <er_chin_ann@hotmail.com>
2023-01-10 16:28:56 +0000

    footer and btt btn

--- project3_frontend/src/App.js
+++ project3_frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
+import "bootstrap-icons/font/bootstrap-icons.css"
 import 'react-toastify/dist/ReactToastify.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -37,6 +38,22 @@ export default function App() {
         />
       </div>
 
+      <a className="backToTop rounded-pill px-2" href="#"><i class="bi bi-arrow-bar-up"></i></a>
+      <footer>
+        <div className="footer-content">
+          <h3>The Struggle Is Real</h3>
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item"><a className="onHoverFooter" style={{ color: "whitesmoke" }} href="#"><i className="bi bi-facebook"></i></a></li>
+            <li className="breadcrumb-item"><a className="onHoverFooter" style={{ color: "whitesmoke" }} href="#"><i className="bi bi-twitter"></i></a></li>
+            <li className="breadcrumb-item"><a className="onHoverFooter" style={{ color: "whitesmoke" }} href="#"><i className="bi bi-google"></i></a></li>
+            <li className="breadcrumb-item"><a className="onHoverFooter" style={{ color: "whitesmoke" }} href="#"><i className="bi bi-youtube"></i></a></li>
+            <li className="breadcrumb-item"><a className="onHoverFooter" style={{ color: "whitesmoke" }} href="#"><i className="bi bi-linkedin"></i></a></li>
+          </ol>
+        </div>
+        <div className="footer-bottom d-flex justify-content-between">
+          <p className="m-0 p-0">Copyright &copy;2021 <a href="#">The Struggle Is Real LLC</a>  </p>
+        </div>
+      </footer>
 
     </React.Fragment>
 
