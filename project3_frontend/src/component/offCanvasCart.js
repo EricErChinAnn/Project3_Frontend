ef590a5a528bf84236eeb8dc69f79264cd1a941e
Eric Er Chin Ann <er_chin_ann@hotmail.com>
2023-01-11 12:56:33 +0000

    Total Cost for each product

--- project3_frontend/src/component/offCanvasCart.js
+++ project3_frontend/src/component/offCanvasCart.js
@@ -36,15 +36,15 @@ export default function OffCanvasCart() {
 
                         {e.product.images.map((each, index) => {
                             if (index == 0) {
-                                return (<img src={each.image_url} key={index} className="card-img-top" style={{width:"100px",height:"auto",objectFit:"scale-down"}} alt="..." />)
+                                return (<img src={each.image_url} key={index} className="card-img-top" style={{ width: "100px", height: "auto", objectFit: "scale-down" }} alt="..." />)
                             }
                         })}
 
                         <div className="card-body">
                             <h5 className="card-title">{e.product.name}</h5>
-                            <p className="card-text m-0 p-0">S${e.product.cost / 100}</p>
+                            <p className="card-text m-0 p-0">S$ {e.product.cost / 100}</p>
                             <p className="card-text m-0 p-0">Quantity: {e.quantity}</p>
-                            <p className="card-text m-0 p-0">Total: S${(e.product.cost / 100)*e.quantity}</p>
+                            <p className="card-text m-0 mt-3 p-0">Total: S$ {(e.product.cost / 100) * e.quantity}</p>
                             <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
